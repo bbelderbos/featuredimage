41da316f6252d7dc550db0242280823050315a81
Bob Belderbos <bobbelderbos@gmail.com>
2013-01-13 12:30:40 +0100

    fixed illegal char in google plus btn

--- footer.php
+++ footer.php
@@ -3,7 +3,7 @@
   <li><a href="#" target="_blank">about</a></li><br>
   <li class="shareIcon"><a title="tweet about this app" href="http://twitter.com/home?status=<?php echo $appUrl; ?>" target="_blank"><img src="http://bobbelderbos.com/wp-content/themes/bamboo/i/twitter-icon.png"></a></li>
   <li class="shareIcon"><a title="share on facebook" href="http://facebook.com/sharer.php?u=<?php echo $appUrl; ?>" target="_blank"><img src="http://bobbelderbos.com/wp-content/themes/bamboo/i/facebook-icon.png"></a></li>
-  <li class="shareIcon"><a title="share on google+" href="#" onclick="return googleplusbtn(\'<?php echo $appUrl; ?>\')"><img src="http://bobbelderbos.com/wp-content/themes/bamboo/i/plus-icon.png" alt="Share on Google+" /></a></li>
+  <li class="shareIcon"><a title="share on google+" href="#" onclick="return googleplusbtn('<?php echo $appUrl; ?>')"><img src="http://bobbelderbos.com/wp-content/themes/bamboo/i/plus-icon.png" alt="Share on Google+" /></a></li>
 </ul>
 <?php include 'js.php'; ?>
 </body>
