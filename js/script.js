db2ba7305559d4266bfd008362d9cc4eb907b5b1
Bob Belderbos <bobbelderbos@gmail.com>
2013-01-13 12:13:49 +0100

    share buttons

--- js/script.js
+++ js/script.js
@@ -151,3 +151,10 @@ function input_is_url(str){
     return false; 
   }
 }
+
+function googleplusbtn(url) {                                                                                                                     
+  sharelink = "https://plus.google.com/share?url="+url;
+  newwindow=window.open(sharelink,'name','height=400,width=600');
+  if (window.focus) {newwindow.focus()}
+  return false;
+}
--- login.php
+++ login.php
@@ -1,6 +1,6 @@
 <div id="login">
   <?php if ($user): ?>
-    <div style='border-bottom: 2px solid #ccc;'>
+    <div>
       Welcome back, <?php echo $user_profile['first_name']; ?> <small>( <a href="<?php echo $logoutUrl; ?>">logout</a> )</small>
     </div>
   <?php endif ?>
