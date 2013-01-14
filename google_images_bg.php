d2096deb34233c2f8f4089d7b28fd737b1caf589
Bob Belderbos <bobbelderbos@gmail.com>
2013-01-14 23:02:57 +0100

    feedback when google no result

--- google_images_bg.php
+++ google_images_bg.php
@@ -3,12 +3,16 @@ include 'google_image_api.php';
 
 $entry = array();
 $i = 0;
-foreach($json->responseData->results as $img){
-  $i++;
-  $entry['id'] = $i;
-  # wtf??! needs to have a value elem: http://stackoverflow.com/questions/13074997/jquery-ui-autocomplete-not-populating-from-remote-datasource
-  $entry['value'] = $img->tbUrl; # smaller pic
-  array_push($return_arr, $entry); 
+if($json->responseData->results){
+  foreach($json->responseData->results as $img){
+    $i++;
+    $entry['id'] = $i;
+    # wtf??! needs to have a value elem: http://stackoverflow.com/questions/13074997/jquery-ui-autocomplete-not-populating-from-remote-datasource
+    $entry['value'] = $img->tbUrl; # smaller pic
+    array_push($return_arr, $entry); 
+  }
+} else {
+  array_push($return_arr, "No results found");
 }
 
 echo json_encode($return_arr);
