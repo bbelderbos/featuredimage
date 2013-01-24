cfea2056de2e3f928efb41d07977f449c0506437
Bob Belderbos <bobbelderbos@gmail.com>
2013-01-24 02:02:28 +0100

    show stored images not only when GET / submitted

--- formsubmit.php
+++ formsubmit.php
@@ -1,6 +1,6 @@
 <?php
-# form (start simple)
 $logfile = "user_images/${user}_images.txt";
+
 if(isset($_GET['bg1_url']) || isset($_GET['bg2_url']) || isset($_GET['overlay_url'])){
   $bgcolor = $_GET['bgcolor'];
   $title["text"] = $_GET['title'];
@@ -17,28 +17,28 @@ if(isset($_GET['bg1_url']) || isset($_GET['bg2_url']) || isset($_GET['overlay_ur
   $images["overlay"]["position"] = $_GET["overlay_pos"];
   $images["overlay"]["size"] = $_GET["overlay_size"];
   $images["overlay"]["opacity"] = $_GET["overlay_opacity"];
+}
 
-  # get stored urls
-  if($user){
-    $storedUrls = array();
-    if(is_file($logfile)){
-      $file = fopen($logfile, "r") or exit("Unable to open file!");
-      while(!feof($file)) {
-        $url = fgets($file);
-        array_push($storedUrls, $url);
-      }
-      fclose($file);
+# get stored urls
+if($user){
+  $storedUrls = array();
+  if(is_file($logfile)){
+    $file = fopen($logfile, "r") or exit("Unable to open file!");
+    while(!feof($file)) {
+      $url = fgets($file);
+      array_push($storedUrls, $url);
     }
+    fclose($file);
+  }
 
-    # write the entry to file if storeLink was clicked and user is logged in with fb
-    if(isset($_GET["storeLink"]) && $user){ 
-      $actual_link = $_SERVER["REQUEST_URI"];
-      $actual_link = str_replace("&storeLink=1", "", $actual_link); # kick this var out
-      if(!in_array($actual_link, $storedUrls)){
-        $fh = fopen($logfile, 'a') or die("can't open file");
-        fwrite($fh, $actual_link."\n");
-        fclose($fh);
-      }
+  # write the entry to file if storeLink was clicked and user is logged in with fb
+  if(isset($_GET["storeLink"]) && $user){ 
+    $actual_link = $_SERVER["REQUEST_URI"];
+    $actual_link = str_replace("&storeLink=1", "", $actual_link); # kick this var out
+    if(!in_array($actual_link, $storedUrls)){
+      $fh = fopen($logfile, 'a') or die("can't open file");
+      fwrite($fh, $actual_link."\n");
+      fclose($fh);
     }
   }
 }
