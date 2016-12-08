7ca6963fbefac468cc25541c20ac88ec88a8ca37
Bob Belderbos <bbelderbos@users.noreply.github.com>
2016-12-09 00:16:05 +0100

    script to add collections of thumbs / full images

---
+++ resize_images.py
@@ -0,0 +1,67 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*- 
+from __future__ import print_function
+import glob
+import os
+import re
+import sys
+from PIL import Image
+ 
+IMAGES = "images"
+THUMB = "_thumb"
+FULL = "_full"
+
+class Portfolio:
+
+  def __init__(self, directory):
+    self.directory = os.path.join(IMAGES, directory)
+    self.imageQueue = list(self._get_images_to_process())
+
+  def _get_images_to_process(self):
+    for im in glob.glob(os.path.join(self.directory, "*")):
+      if THUMB in im or FULL in im:
+        continue
+      yield im
+
+  def resize_image(self, image, width, thumb):
+    """ modified source from http://pythoncentral.io/resize-image-python-batch/ """
+    try:
+      width = int(width)
+    except:
+      raise ValueError("Provide width (%s) is not an integer value" % str(width))
+    if not os.path.isfile(image):
+      raise IOError("Image path not found: %s" % image)
+    try:
+      img = Image.open(image)
+    except: 
+      raise IOError("Not an image file, or cannot process it")
+    # Calculate the height using the same aspect ratio
+    widthPercent = (width / float(img.size[0]))
+    if thumb:
+      height = width
+    else:
+      height = int((float(img.size[1]) * float(widthPercent)))
+    img = img.resize((width, height), Image.BILINEAR)
+    if "." in image:
+      name, ext = os.path.splitext(image)
+    else:
+      name, ext = image, ""
+    
+    outFile = name + (THUMB if thumb else FULL) + ext
+    img.save(outFile)
+    return outFile
+
+if __name__ == "__main__":
+  if len(sys.argv) < 4:
+    sys.exit("%s dirname thumbWidth fullWidth" % sys.argv[0])
+  dirname = sys.argv[1]
+  thumbWidth = sys.argv[2]
+  fullWidth = sys.argv[3]
+  p = Portfolio(dirname)
+  for im in p.imageQueue:
+    try:
+      thumbImg = p.resize_image(im, thumbWidth, thumb=True)
+      zoomImg = p.resize_image(im, fullWidth, thumb=False)
+      print("OK: thumb img: %s, zoom img: %s" % (thumbImg, zoomImg))
+    except Exception as exc:
+      print("ERR: processing %s, exception: %s" % (im, exc))
